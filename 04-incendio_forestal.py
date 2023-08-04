c00aa8aa96a951a6a7f63a79dca6ee91dc4fea3d
ndevbogado <ndevbogado@gmail.com>
2023-08-03 23:30:01 -0300

    feat: add 'dinamica' to incendio forestal

--- 04-incendio_forestal.py
+++ 04-incendio_forestal.py
@@ -19,9 +19,10 @@ def suceso_aleatorio(n):
     
 def brotes(bosque, p):
     for i in range(len(bosque)):
-        chance = suceso_aleatorio(100)
-        if chance <= p:
-            bosque[i] = 1
+        if bosque[i] == 0:
+            chance = suceso_aleatorio(100)
+            if chance <= p:
+                bosque[i] = 1
     return bosque    
 
 def cuantos(bosque,tipo_celda):
@@ -61,12 +62,34 @@ def limpieza(bosque):
             bosque[i] = 0
     return bosque
 
-def dinamica():
-    
+def dinamica(tamanio_bosque, anios, prob_brote, prob_rayo):
+    bosque = generador_bosque(tamanio_bosque)
+    print(f"Generador del bosque: {bosque}")
+    registro_sobrevivientes = []
+    for i in range(anios):
+        contador_arboles = 0
+        bosque = brotes(bosque, prob_brote)
+        print(f"Epoca de brote: {bosque}")
+        bosque = rayos(bosque, prob_rayo)
+        print(f"Epoca de caida de rayos: {bosque}")
+        bosque = propagacion(bosque)
+        print(f"Periodo de propagacion del fuego: {bosque}")
+        bosque = limpieza(bosque)
+        print(f"Epoca de limpieza: {bosque}")
+        cant_arboles_sup = sum(bosque)
+        print(f"Arboles sobrevivientes: {cant_arboles_sup}")
+        registro_sobrevivientes.append(cant_arboles_sup)
+        print(f"### FINAL DEL anio {i+1} ###")
+    print(f"\nregistro de sobrevivientes: {registro_sobrevivientes}")
+    promedio_sobrevivientes = sum(registro_sobrevivientes) / len(registro_sobrevivientes)
+    print(f"promedio: {promedio_sobrevivientes}")
+    return promedio_sobrevivientes
 
+dinamica(20,500,3,2)
+"""
 prop = propagacion([1, 1, 1, -1, 0, 0, 0, 1, 1, 0])
 print(prop)
 prop = limpieza(prop)
 print(prop)
-
+"""
     
